b7b835893cb1788868d1efc2540d801be1d9231c
alib5t <alimehel157@gmail.com>
2026-05-03 12:21:07 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const TxtEditorApp());
